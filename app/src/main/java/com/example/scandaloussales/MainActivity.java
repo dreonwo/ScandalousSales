f1d942d6aedb817f83542fed50e3226b716c772c
dreonwo <dwheatleyowens@icloud.com>
2021-04-20 16:32:36 -0400

    Model Class Added

--- app/src/main/java/com/example/scandaloussales/MainActivity.java
+++ app/src/main/java/com/example/scandaloussales/MainActivity.java
@@ -17,23 +17,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Post post = new Post();
-//
-//        post.setItemName("Chocolate");
-//        post.setPrice(23);
-//        post.setUpc(1339270837);
-//        post.setUser(ParseUser.getCurrentUser());
-//
-//        post.saveInBackground(new SaveCallback() {
-//            @Override
-//            public void done(ParseException e) {
-//                if(e != null){
-//                    Log.e("MainActivity", "Error while saving", e);
-//                    Toast.makeText(MainActivity.this, "Error while saving!", Toast.LENGTH_SHORT).show();
-//                }
-//
-//                Log.i("MainActivity", "Post save was successful!");
-//            }
-//        });
     }
 }
