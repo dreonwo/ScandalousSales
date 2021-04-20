f614056e9b9b69666a2802e07a617adbe23d2f36
dreonwo <dwheatleyowens@icloud.com>
2021-04-20 16:30:09 -0400

    Added Model class

--- app/src/main/java/com/example/scandaloussales/MainActivity.java
+++ app/src/main/java/com/example/scandaloussales/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.scandaloussales;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Post post = new Post();
+//
+//        post.setItemName("Chocolate");
+//        post.setPrice(23);
+//        post.setUpc(1339270837);
+//        post.setUser(ParseUser.getCurrentUser());
+//
+//        post.saveInBackground(new SaveCallback() {
+//            @Override
+//            public void done(ParseException e) {
+//                if(e != null){
+//                    Log.e("MainActivity", "Error while saving", e);
+//                    Toast.makeText(MainActivity.this, "Error while saving!", Toast.LENGTH_SHORT).show();
+//                }
+//
+//                Log.i("MainActivity", "Post save was successful!");
+//            }
+//        });
     }
 }
---
+++ app/src/main/java/com/example/scandaloussales/Post.java
@@ -0,0 +1,56 @@
+package com.example.scandaloussales;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import java.util.Date;
+
+@ParseClassName("Post")
+public class Post extends ParseObject {
+    public static final String KEY_ITEM_NAME = "itemName";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_USER = "userId";
+    public static final String KEY_CREATED_KEY = "createdAt";
+    public static final String KEY_PRICE = "itemPrice";
+    public static final String KEY_UPC = "upc";
+
+    public String getItemName(){
+        return getString(KEY_ITEM_NAME);
+    }
+
+    public void setItemName(String itemName){
+        put(KEY_ITEM_NAME, itemName);
+    }
+
+    public ParseFile getImage(){
+        return getParseFile(KEY_IMAGE);
+    }
+
+    public void setImage(ParseFile parseFile){
+        put(KEY_IMAGE,parseFile);
+    }
+
+    public ParseUser getUser(){
+        return getParseUser(KEY_USER);
+    }
+
+    public Date getCreatedAt(){ return getDate(KEY_CREATED_KEY); }
+
+    public void setUser(ParseUser user){
+        put(KEY_USER, user);
+    }
+
+    public String getPrice() {
+        return getString(KEY_PRICE);
+    }
+
+    public void setPrice(int price){ put(KEY_PRICE,price);}
+
+    public String getUpc() {
+        return getString(KEY_UPC);
+    }
+
+    public void setUpc(int upc){put(KEY_UPC, upc);}
+}
